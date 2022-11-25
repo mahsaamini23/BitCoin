b555d4fd8066998721e14e3a1ca1710c06ecc76e
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-25 15:03:24 +0330

    delete import color

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import { grey } from "@mui/material/colors";
-
 
 
 const Card =({key, name , image , price, change , color,Icon})=>{
