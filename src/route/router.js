7d94985e00c01d10581746ffb93efedfb1159bc3
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-25 14:57:22 +0330

    add router and path

--- src/route/router.js
+++ src/route/router.js
@@ -1,14 +1,25 @@
-import { createBrowserRouter } from "react-router-dom";
+import React from "react";
+import {createBrowserRouter} from "react-router-dom";
+import Home from "page/Home/Home";
+import Layout from "component/Layout/Layout";
+import CurrentPrice from "page/CurrentPrice/CurrentPrice";
 
 
-const Router = createBrowserRouter([
+export const Router = createBrowserRouter([
     {
-        path:'/',
-        element:"",
-    },
-    {
-        path:'',
-        element:"",
+        path:"/",
+        element:<Layout/>,
+        children:[
+            {
+                index:true,
+                element:<Home/>
+            },
+            {
+                path:"/currentPrice",
+                element:<CurrentPrice/>
+            }
+            
+        ]
     }
 ])
 
