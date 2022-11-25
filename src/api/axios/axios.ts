f73c69f96cb00d193bfb25e1a96feff839ad90ea
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-25 14:55:58 +0330

    delete file

--- src/api/axios/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-    baseURL:'https://coinranking1.p.rapidapi.com'
-})
-
-export const handleGetDataCoin = () =>{
-    return api(`/coins`).then(data => data)
-}
