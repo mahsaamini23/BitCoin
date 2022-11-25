d0a9208eee4d888a0bab9ee19fda3c3fd071585b
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-25 14:59:43 +0330

    create item

---
+++ src/component/Item/Item.jsx
@@ -0,0 +1,16 @@
+import React from  "react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+const Item =({children, nameIcon})=>{
+    return (
+        <Grid container item sx={{display:'flex' , flexDirection:'column', justifyContent:'space-evenly',width:'25%' , alignContent:'baseline',padding:'0 10px'}} >
+            {children}
+            <Typography sx={{color:'grey',textAlign:'center'}}>
+                {nameIcon}
+            </Typography>
+        </Grid>
+    )
+}
+
+export default Item;
