8fb0d8240b4737a7f0866e5de78da5ce56f7d897
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-25 14:58:02 +0330

    add styled for textField

---
+++ src/styles/styleTextField.js
@@ -0,0 +1,25 @@
+import {styled} from "@mui/material";
+import TextField from "@mui/material/TextField";
+
+const StyledTextField = styled(TextField)(({theme})=>({
+    height:44,
+    '& .MuiTextField-root':{
+        color:'grey',
+    },
+    '& .MuiOutlinedInput-root': {
+        width: 220,
+        height: 40,
+        color:'grey',
+        
+        '& fieldset': {
+            borderColor: 'grey',
+        },
+    },
+    '&.Mui-focused fieldset': {
+        border: '1px solid black',
+        borderColor:'grey',
+    },    
+}
+))
+
+export default StyledTextField;
