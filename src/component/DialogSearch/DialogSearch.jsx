79aad783283e643f41c14a59b8679ed02cccf47a
ms.moradi23 <ms.moradi23@yahoo.com>
2022-12-02 02:23:07 +0330

    add logic

--- src/component/DialogSearch/DialogSearch.jsx
+++ src/component/DialogSearch/DialogSearch.jsx
@@ -2,7 +2,8 @@ import react , {useState} from "react";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
-import Input from "@mui/material/Input";
+import InputAdornment from '@mui/material/InputAdornment';
+import TextField from '@mui/material/TextField';
 import Grid from "@mui/material/Grid";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -11,6 +12,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
 import SearchIcon from '@mui/icons-material/Search';
+
 
 
 const DialogSearch =({open, setOpen, coins, selectFees, setSelectFees, setTextField})=>{
@@ -25,9 +27,10 @@ const DialogSearch =({open, setOpen, coins, selectFees, setSelectFees, setTextFi
     }
 
     const handleSelectFees =(id)=>{
-        setTextField({Toman:'', unite:'', name:''})
+        setTextField({Toman:'', name:''})
         setSelectFees(coins.filter(coin => coin.id === id))
-        setTextField({Toman:selectFees[0].current_price, unite:1, name:selectFees[0].name})
+        setTextField({Toman:selectFees[0].current_price, name:selectFees[0].name})
+        setOpen(false)
     }
 
    
@@ -35,17 +38,26 @@ const DialogSearch =({open, setOpen, coins, selectFees, setSelectFees, setTextFi
         <Dialog open={open} onClose={handleClose}>
             <DialogTitle>انتخاب ارز</DialogTitle>
             <DialogContent>
-                <Grid container item sx={{display:'flex',alignItems:'center',gap:'10px',border:'1px solid #e0e0e0', borderRadius:'20px' }}>
-                    <SearchIcon sx={{fontSize:25, padding:'0 5px'}}/> 
-                    <Input sx={{width:'85%', height:'40px'}} type='search' onChange={handleSearchFees}/>
+                <Grid container item >
+                    
+                    <TextField sx={{width:'100%', height:'35px'}} id="search" onChange={handleSearchFees} size="small"
+                    InputProps={{
+                        startAdornment:(
+                            <InputAdornment position='start'>
+                                <SearchIcon/>
+                            </InputAdornment>
+                        ) 
+                    }}
+                    variant='outlined'
+                    />
                 </Grid>
-                <TableContainer >
+                <TableContainer  mt={6}>
                     <Table sx={{width:'400px', height:'550px'}}>
                         <TableBody>
                             {coins.filter(coin => searchFees === '' ? coin : coin.name.toLowerCase().includes(searchFees.toLowerCase()))
                             .map((coin) => (
                                 <TableRow  key={coin.id} sx={{width:'380px',height:'60px',display:'flex', justifyContent:'space-between'}} onClick={()=>handleSelectFees(coin.id)}>
-                                    <TableCell align='right' sx={{width:'50%', display:'flex', justifyContent:'flex-end', alignItems:'center', gap:'10px'}}>{coin.name}<img src={coin.image} alt="logo" style={{width:'25px', height:'25px'}}/></TableCell>
+                                    <TableCell align='right' sx={{width:'50%', display:'flex', justifyContent:'flex-end', alignItems:'center', gap:'10px', cursor:'pointer'}}>{coin.name}<img src={coin.image} alt="logo" style={{width:'25px', height:'25px'}}/></TableCell>
                                     <TableCell align='right' sx={{width:'50%', display:'flex', flexDirection:'column',alignItems:'center', gap:'10px'}}>
                                         <Grid>
                                             <Typography>قیمت خرید</Typography>
